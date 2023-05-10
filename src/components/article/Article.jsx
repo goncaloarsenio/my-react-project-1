f97988cc44441bc4c75bbe0f8dab585f3f6613dd
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-05-10 11:09:46 +0100

    blog-articles change

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./article.css";
 
-const Article = ({ imgUrl, date, title }) => {
+const Article = ({ imgUrl, date, title, url, url2, url3, url4, url5 }) => {
   return (
     <div className="gpt3__blog-container_article ">
       <div className="gpt3__blog-container_article-image">
@@ -11,8 +11,58 @@ const Article = ({ imgUrl, date, title }) => {
         <div>
           <p>{date}</p>
           <h3>{title}</h3>
+
+          <h5>
+            {" "}
+            <a
+              href="https://towardsdatascience.com/gpt-3-the-good-the-bad-and-the-ugly-5e2e5b7f0f66"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url}
+            </a>{" "}
+          </h5>
+          <h5>
+            {" "}
+            <a
+              href="https://www.articleforge.com/blog/why-we-dont-use-gpt-3/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url2}
+            </a>{" "}
+          </h5>
+          <h5>
+            {" "}
+            <a
+              href="https://www.technologyreview.com/2021/02/24/1017797/gpt3-best-worst-ai-openai-natural-language/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url3}
+            </a>{" "}
+          </h5>
+          <h5>
+            {" "}
+            <a
+              href="https://www.forbes.com/sites/robtoews/2020/07/19/gpt-3-is-amazingand-overhyped/?sh=4be78d0e1b1c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url4}
+            </a>{" "}
+          </h5>
+          <h5>
+            {" "}
+            <a
+              href="https://www.trendmicro.com/en_fi/research/23/b/review-what-gpt-3-taught-chatgpt-in-a-year.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url5}
+            </a>{" "}
+          </h5>
         </div>
-        <p>Read Full Article</p>
       </div>
     </div>
   );
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -15,30 +15,35 @@ const Blog = () => {
         <div className="gpt3__blog-container_groupA">
           <Article
             imgUrl={blog01}
-            date="Sep 26, 2021"
-            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Sep 24, 2020"
+            title="GPT-3: The good, the bad and the ugly"
+            url="Read the full article"
           />
         </div>
         <div className="gpt3__blog-container_groupB">
           <Article
             imgUrl={blog02}
-            date="Sep 26, 2021"
-            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Nov 22, 2022"
+            title='"Why we do not use GPT-3"'
+            url2="Read the full article"
           />{" "}
           <Article
             imgUrl={blog03}
-            date="Sep 26, 2021"
-            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Feb 24, 2021"
+            title="Why GPT-3 is the best and worst of AI"
+            url3="Read the full article"
           />{" "}
           <Article
             imgUrl={blog04}
-            date="Sep 26, 2021"
-            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Jul 19, 2020"
+            title="GPT-3 Is Amazing—And Overhyped"
+            url4="Read the full article"
           />{" "}
           <Article
             imgUrl={blog05}
-            date="Sep 26, 2021"
-            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Feb 21, 2023"
+            title="What GPT-3 Taught ChatGPT in a Year"
+            url5="Read the full article"
           />
         </div>
       </div>
